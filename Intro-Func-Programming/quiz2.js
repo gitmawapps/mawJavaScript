8585ec22ef8ad99e942dd4ec2d50f7008c41f74a
Mark Daniels <mark.daniels@email.cerrocoso.edu>
2015-09-26 14:00:02 -0700

    First commit

---
+++ Intro-Func-Programming/quiz2.js
@@ -0,0 +1,13 @@
+var musicians = [
+  {name: "Louis Armstrong", genre: "Jazz", birthday: "August 4, 1901"},
+  {name: "Miles Davis", genre: "Jazz", birthday: "May 26, 1926"},
+  {name: "Dizzy Gillespie", genre: "Jazz", birthday: "October 21, 1917" },
+  {name: "Michael Jackson", genre: "RnB", birthday: "August 29, 1958"},
+  {name: "Mary J Blige", genre: "RnB", birthday: "January 11, 1971"},
+  {name: "Katy Perry", genre: "Pop", birthday: "October 25, 1984"},
+  {name: "Justin Bieber", genre: "Pop", birthday: "March 1, 1994"}
+  ];
+  
+// Complete the pieces of code below; Use the map function
+
+var birthdays = musicians.map();
